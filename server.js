409a13771ff84998355da868af6a277f41da36c2
ashminbhandari <abhandar@ramapo.edu>
2020-06-08 21:24:07 -0400

    graphql request/queries/helloworld

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const { gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+    schema {
+        query: Query
+    }
+
     type Query {
         greeting: String
     }
 `;
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello GraphQL world'
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen({port: 9000}).then(({url}) => console.log(`Server running at: ${url}`));
